c3fec2e323c543caa705d99167a7b86dbdbdce94
Gabriel Luezas <130647529+GabrielLuezas@users.noreply.github.com>
2025-10-16 11:08:06 +0200

    Ejercicio 10

---
+++ Tema4/Practicas/ejercicio10.js
@@ -0,0 +1,19 @@
+/* Escriba una función que tome un número decimal como entrada, y
+devuelva el número de bits que son iguales a uno en la
+representación binaria de ese número. Comprueba que la entrada no
+sea negativa.
+*/
+
+
+function ejercicio9(numeros){
+
+    let binario = numeros.toString(2);
+
+
+    return binario
+
+}
+
+
+
+console.log(ejercicio9(25))
