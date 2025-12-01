b077b2fde6fccb664b9bf98830f55dd0cb8c433a
Gabriel Luezas <gabrieluezas@gmail.com>
2025-12-01 20:35:11 +0100

    Salvarme el culo por si suspendo XDJAJA

--- Unidad5/Apuntes/PosibleEjercicio/Usuario.js
+++
@@ -1,8 +0,0 @@
-class Usuario {
-  constructor(usuario, contrasena, rol = "usuario") {
-    this.usuario = usuario;
-    this.contrasena = contrasena;
-    this.rol = rol; // 'usuario' or 'admin'
-    this.puntuacion = 0;
-  }
-}
